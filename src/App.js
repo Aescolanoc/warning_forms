5bee36280004a8979459e5fa4bb74bce934413b3
Mel Escolano <amalia.escolano@gmail.com>
2022-05-05 16:45:50 +0200

    Added basics components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AddUser from './components/Users/AddUser';
+
+
+function App() {
+  return (
+    <div>
+      <AddUser></AddUser>
+    </div>
+  );
+}
+
+export default App;
